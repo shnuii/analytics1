df0ee4e1bc6329fafe4ea0b83c4ff3ac028ec606
shnuii <vasireddyvishnupriya@gmail.com>
2018-08-20 00:29:35 +0530

    19/08-blah2

---
+++ excercise1.R
@@ -0,0 +1,19 @@
+library(gsheet)
+
+url='https://docs.google.com/spreadsheets/d/1h7HU0X_Q4T5h5D1Q36qoK40Tplz94x_HZYHOJJC_edU/edit#gid=216113907'
+denco = as.data.frame(gsheet2tbl(url))
+str(denco)
+head(denco)
+library(dplyr)
+denco %>% count(custname, sort = TRUE)
+sales %>% group_by(custname) %>% summarise(n=n()) %>% arrange(desc(n))
+
+df3a = aggregate(denco$revenue, by = list(denco$partnum), FUN=sum)
+head(df3a)
+
+denco %>% group_by(partnum) %>% summarise(n=n()) %>% arrange(desc(n))
+
+names(sales)
+df4a = aggregate(margin ~ partnum, data=sales, FUN=sum)
+aggregate(margin ~ partnum, data=sales, FUN=sum)
+head(df4a[order(df4a$margin, decreasing =T),])
